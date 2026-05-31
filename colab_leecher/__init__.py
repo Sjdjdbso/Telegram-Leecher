ead26f4ed8927c4459f1486d71bab71225a230fe
Leonardo_ <168467152+Sjdjdbso@users.noreply.github.com>
2026-06-01 05:25:39 +0700

    Update __init__.py

--- colab_leecher/__init__.py
+++ colab_leecher/__init__.py
@@ -1,12 +1,16 @@
 # copyright 2023 © Xron Trix | https://github.com/Xrontrix10
-import asyncio
-import logging, json
-from uvloop import install
-from pyrogram.client import Client
 
-# Read the dictionary from the txt file
-with open("/content/Telegram-Leecher/credentials.json", "r") as file:
-    credentials = json.loads(file.read())
+import asyncio
+import logging
+import json
+from pyrogram import Client
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+# Load credentials
+with open("/content/tgdl/credentials.json", "r") as file:
+    credentials = json.load(file)
 
 API_ID = credentials["API_ID"]
 API_HASH = credentials["API_HASH"]
@@ -20,5 +24,5 @@ try:
 except RuntimeError:
     asyncio.set_event_loop(asyncio.new_event_loop())
     
-
+# Create Pyrogram client
 colab_bot = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
